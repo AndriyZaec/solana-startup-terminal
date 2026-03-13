73b6c4bf2ee7ee3e2bb5ea33d373271661719106
Andrew Zaiets <andrew.zaec@gmail.com>
2026-03-13 22:29:56 +0200

    Test check PDA for minted NFT

--- accounts/src/main.rs
+++ accounts/src/main.rs
@@ -6,11 +6,14 @@ use solana_cli_config::Config;
 use solana_client::{rpc_client::RpcClient, rpc_config::CommitmentConfig};
 use solana_sdk::{
     clock,
+    program_pack::Pack,
     pubkey::Pubkey,
     signature::{Keypair, Signer},
     signer::EncodableKey,
     sysvar::SysvarId,
+    transaction::Transaction,
 };
+use solana_system_interface::instruction::create_account;
 use std::str::FromStr;
 
 fn main() -> anyhow::Result<()> {
@@ -24,7 +27,7 @@ fn main() -> anyhow::Result<()> {
     let client =
         RpcClient::new_with_commitment(cfg.json_rpc_url.clone(), CommitmentConfig::confirmed());
 
-    let sender = Keypair::read_from_file(cfg.keypair_path)
+    let sender = Keypair::read_from_file("local_wallet.json")
         .map_err(|e| anyhow!("Failed to read keypair from file: {}", e))?;
 
     // 1. Fetch sender
@@ -40,13 +43,21 @@ fn main() -> anyhow::Result<()> {
 
     // 3. Fetching metaplex metadata pda (mainnet address! - спробуйте запустина на devnet)
     let metaplex_program_id = mpl_token_metadata::ID;
-    let usdc_id = Pubkey::from_str("Es9vMFrzaCERmJfrF4H2FYD4KCoNkY11McCe8BenwNYB")?;
-    let metadata_seeds = &[b"metadata", metaplex_program_id.as_ref(), usdc_id.as_ref()];
+    let usdc_id = Pubkey::from_str("4zMMC9srt5Ri5X14GAgXhaHii3GnPAEERYPJgZJDncDU")?;
+    let mint_pub = create_nft(&client, sender)?;
+
+    let metadata_seeds = &[
+        b"metadata",
+        mpl_token_metadata::ID.as_ref(),
+        mint_pub.as_ref(),
+    ];
 
     let (metadata_key, metadata_bump) =
         Pubkey::find_program_address(metadata_seeds, &metaplex_program_id);
 
-    let metadata_acc = client.get_account(&metadata_key)?;
+    let metadata_acc = client
+        .get_account(&metadata_key)
+        .map_err(|e| anyhow!("No PDA acc for this {e}"))?;
     let decoded = Metadata::deserialize(&mut &metadata_acc.data[..])?;
 
     println!("\n========================================");
@@ -55,4 +66,130 @@ fn main() -> anyhow::Result<()> {
     println!("Metadata account: {}-{}", decoded.name, decoded.symbol);
 
     Ok(())
+}
+
+fn create_nft(client: &RpcClient, wallet: Keypair) -> anyhow::Result<Pubkey> {
+    let mint = Keypair::new();
+    print!("mint: {0}", mint.pubkey());
+
+    let recent_blockhash = client.get_latest_blockhash()?;
+    let rent = client.get_minimum_balance_for_rent_exemption(spl_token::state::Mint::LEN)?;
+
+    let create_mint_account_ix = create_account(
+        &wallet.pubkey(),
+        &mint.pubkey(),
+        rent,
+        spl_token::state::Mint::LEN as u64,
+        &spl_token::id(),
+    );
+
+    let create_mint_tx = Transaction::new_signed_with_payer(
+        &[create_mint_account_ix],
+        Some(&wallet.pubkey()),
+        &[&wallet, &mint],
+        recent_blockhash,
+    );
+
+    client.send_and_confirm_transaction(&create_mint_tx)?;
+
+    let init_mint_ix = spl_token::instruction::initialize_mint(
+        &spl_token::id(),
+        &mint.pubkey(),
+        &wallet.pubkey(),
+        Some(&wallet.pubkey()),
+        0,
+    )?;
+
+    let init_mint_tx = Transaction::new_signed_with_payer(
+        &[init_mint_ix],
+        Some(&wallet.pubkey()),
+        &[&wallet],
+        recent_blockhash,
+    );
+
+    client.send_and_confirm_transaction(&init_mint_tx)?;
+
+    let ata = spl_associated_token_account::get_associated_token_address(
+        &wallet.pubkey(),
+        &mint.pubkey(),
+    );
+
+    let create_ata_ix = spl_associated_token_account::instruction::create_associated_token_account(
+        &wallet.pubkey(),
+        &wallet.pubkey(),
+        &mint.pubkey(),
+        &spl_token::id(),
+    );
+
+    let create_ata_tx = Transaction::new_signed_with_payer(
+        &[create_ata_ix],
+        Some(&wallet.pubkey()),
+        &[&wallet],
+        recent_blockhash,
+    );
+
+    client.send_and_confirm_transaction(&create_ata_tx)?;
+
+    let mint_to_ix = spl_token::instruction::mint_to(
+        &spl_token::id(),
+        &mint.pubkey(),
+        &ata,
+        &wallet.pubkey(),
+        &[],
+        1,
+    )?;
+
+    let mint_to_tx = Transaction::new_signed_with_payer(
+        &[mint_to_ix],
+        Some(&wallet.pubkey()),
+        &[&wallet],
+        recent_blockhash,
+    );
+
+    client.send_and_confirm_transaction(&mint_to_tx)?;
+
+    let mint_pubkey = mint.pubkey();
+    let metadata_seeds = &[
+        b"metadata",
+        mpl_token_metadata::ID.as_ref(),
+        mint_pubkey.as_ref(),
+    ];
+
+    let (metadata_pda, _metadata_bump) =
+        Pubkey::find_program_address(metadata_seeds, &mpl_token_metadata::ID);
+
+    println!("\n========================================");
+    println!("NFT Mint key: {mint_pubkey}");
+    println!("\n========================================");
+
+    let data = mpl_token_metadata::types::DataV2 {
+        name: String::from("My Lovely NFT"),
+        symbol: String::from("MLNG"),
+        uri: "https://example.com/metadata.json".to_string(),
+        seller_fee_basis_points: 0,
+        creators: None,
+        collection: None,
+        uses: None,
+    };
+
+    let create_metadata_ix =
+        mpl_token_metadata::instructions::CreateMetadataAccountV3Builder::new()
+            .metadata(metadata_pda)
+            .mint(mint_pubkey)
+            .mint_authority(wallet.pubkey())
+            .payer(wallet.pubkey())
+            .update_authority(wallet.pubkey(), true)
+            .data(data)
+            .is_mutable(true)
+            .instruction();
+
+    let tx = Transaction::new_signed_with_payer(
+        &[create_metadata_ix],
+        Some(&wallet.pubkey()),
+        &[&wallet],
+        recent_blockhash,
+    );
+    client.send_and_confirm_transaction(&tx)?;
+
+    Ok(mint_pubkey)
 }
